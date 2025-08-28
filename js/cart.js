8880f426c3d5499a8cce768059c1c43513f6d904
Squ1nty <vincentdao288@gmail.com>
2025-08-28 13:04:29 +1000

    Touch up for Cart responsiveness. Cart now aligns better with mobile design ranging from 320px to 1024px

--- js/cart.js
+++ js/cart.js
@@ -6,54 +6,54 @@ let profileIconContainer = document.querySelector(".profileIconContainer");
 
 // Functions to set the cart's active/inactive state
 function setCartInactive(){
-    cartPopUp.setAttribute("inert", true);
-    cartPopUp.style.opacity = "0";
+	cartPopUp.setAttribute("inert", true);
+	cartPopUp.style.opacity = "0";
 }
 function setCartActive(){
-    cartPopUp.removeAttribute("inert");
-    cartPopUp.style.opacity = "100%";
+	cartPopUp.removeAttribute("inert");
+	cartPopUp.style.opacity = "100%";
 }
 
 // Handles where the "Tab" key should direct a user based off cartPopUp being active or not
 function handleCartTabbing(){
-    if(cartPopUp.style.opacity === "0"){
-        profileIconContainer.focus();
-    }
-    else{
-        // In future, set this to the bin icon, if empty, focus stays on Cart Icon?
-        // Additional note: check for the state of the cart not being empty as well?
-        //                  this would ... just said the same thing above lol
-    }
+	if(cartPopUp.style.opacity === "0"){
+		profileIconContainer.focus();
+	}
+	else{
+		// In future, set this to the bin icon, if empty, focus stays on Cart Icon?
+		// Additional note: check for the state of the cart not being empty as well?
+		//                  this would ... just said the same thing above lol
+	}
 }
 
 // Handles which state cart should be set to and calls relevant method
 function handleCartState(){
-    if(cartPopUp.style.opacity === "0"){
-        setCartActive();
-    }
-    else{
-        setCartInactive();
-    }
+	if(cartPopUp.style.opacity === "0"){
+			setCartActive();
+	}
+	else{
+			setCartInactive();
+	}
 }
 
 // Upon loading/refresh of the page, cart will be set to a default inactive state
 window.onload = () => {
-    setCartInactive();
+	setCartInactive();
 }
 
 // Click event listener for cartIcon to set cartPopUp as either active or inactive
 cartIcon.addEventListener("click", (e) => {
-    handleCartState();
+	handleCartState();
 });
 // On top of the above ^
 // Tab event listener for CartIcon conditioned for whether or not the cartPopUp is active
 cartIcon.addEventListener("keydown", (e) => {
-    e.preventDefault();
-    if(e.key === "Enter" || e.key === " "){
-        handleCartState();
-    }
-    else if(e.key === "Tab"){
-        handleCartTabbing();
-    }
+	e.preventDefault();
+	if(e.key === "Enter" || e.key === " "){
+		handleCartState();
+	}
+	else if(e.key === "Tab"){
+		handleCartTabbing();
+	}
 });
 
